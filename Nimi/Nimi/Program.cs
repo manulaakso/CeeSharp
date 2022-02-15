a21be46ab885f87e6bea397132b39b8279ee6c87
manulaakso <manu.laakso@edu.keuda.fi>
2022-02-15 09:53:18 +0000

    ensimmäiset harjoitukset

---
+++ Nimi/Nimi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Manu Laakso");
+        }
+    }
+}
